0198cc7c0854a5a633f605dd3d407198924c4e51
William12445 <waiyanlin2342@gmail.com>
2024-07-11 02:53:20 +0000

    1終了

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def test(request):
+    return HttpResponse("ggggggggggghsfhdhsggg")
